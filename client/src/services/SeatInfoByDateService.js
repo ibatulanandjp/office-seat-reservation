672a5fbf41064437f705f06df732922bd3313e47
Atul Anand <ib.atulanandjp@gmail.com>
2023-07-05 16:25:05 +0900

    Fixed the bug of seat not loading when seat=0

--- client/src/services/SeatInfoByDateService.js
+++ client/src/services/SeatInfoByDateService.js
@@ -15,5 +15,8 @@ export async function getSeatInfoByDate(date) {
   const url = `/api/v1/seat/${date}`;
   const seatInfoByDate = await fetch(url, requestOptions);
   const seatInfoByDateObj = await seatInfoByDate.json();
-  return await seatInfoByDateObj[0].total_seat;
+  if (seatInfoByDateObj.length) {
+    return await seatInfoByDateObj[0].total_seat;
+  }
+  return 0;
 }
